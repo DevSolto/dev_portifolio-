af6926f0a202647a5033a72df7ec2b2e1bba78a5
santiago <santiagosiqueirasouto@gmail.com>
2025-11-04 02:40:36 -0300

    merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
-import {
-  ContactSection,
-  HeroSection,
-  ProjectsSection,
-  ServicesSection,
-  TestimonialsSection,
-} from "@/components/sections";
+
 import AboutSection from "./components/AboutSection";
+import TerminalHero from "./components/TerminalHero";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#0b0f14] text-white">
       <TerminalHero />
+      <AboutSection />
+      
     </main>
   );
 }
